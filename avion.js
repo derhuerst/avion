916cdc0c15666c5a5e315638c69362975e17ac78
Jannis R <mail@jannisr.de>
2016-06-29 16:19:34 +0200

    p2p setup

--- avion.js
+++ avion.js
@@ -1,3 +1,16 @@
 'use strict'
 
-console.log('foo')
+const peer = require('./peer')
+
+
+
+peer.on('error', (e) => {
+	// todo
+})
+peer.on('connect', () => {
+	if (peer.initiator) peer.send('hi')
+	// todo
+})
+peer.on('data', (d) => {
+	// todo
+})

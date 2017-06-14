6b5edca9380c3dae5776c5bcd486b168c1cbd43f
Jannis R <mail@jannisr.de>
2017-06-14 23:27:06 +0200

    connection logic & file handling

--- index.js
+++ index.js
@@ -1,20 +1,79 @@
 'use strict'
 
 const generateId = require('alphanumeric-id')
+const createEndpoint = require('files-sync-stream')
 const diff = require('virtual-dom/diff')
 const patch = require('virtual-dom/patch')
 const createElement = require('virtual-dom/create-element')
 
 const render = require('./lib/ui')
+const fileReader = require('./lib/file-reader')
+const connect = require('./lib/connect')
 
 const state = {
 	id: location.hash.length === 7 ? location.hash.slice(1) : generateId(6),
 	isLeader: location.hash.length === 7,
-	endpoint: null
+	dataPeer: null,
+	signalingPeer: null,
+	endpoint: null,
+	filesToAdd: []
+}
+
+const addFilesToEndpoint = (files) => {
+	for (let file of files) {
+		const handle = state.endpoint.add(fileReader(file), {
+			name: file.name,
+			size: file.size,
+			type: file.type
+		})
+		onFile(handle)
+	}
+}
+
+const onFile = (file, isIncoming = false) => {
+	file.on('start', rerender)
+	file.on('data', (chunk) => {
+		// todo: handle chunk
+		rerender()
+	})
+	file.on('end', rerender)
+}
+
+const init = () => {
+	const d = connect(state.id, 'data', state.isLeader)
+	const s = connect(state.id, 'signaling', state.isLeader)
+
+	const onConnect = () => {
+		if (d.connected && s.connected) {
+			d.removeListener('connect', onConnect)
+			s.removeListener('connect', onConnect)
+		}
+
+		const endpoint = createEndpoint(d, s, state.isLeader)
+		state.endpoint = endpoint
+
+		addFilesToEndpoint(state.filesToAdd)
+		endpoint.on('file', (file) => {
+			onFile(file, true)
+			rerender()
+		})
+		endpoint.on('done', rerender)
+
+		rerender()
+	}
+	d.on('connect', onConnect)
+	s.on('connect', onConnect)
+
+	state.dataPeer = d
+	state.signalingPeer = s
+	rerender()
 }
 
 const addFiles = (files) => {
-	// todo
+	if (state.endpoint) addFilesToEndpoint(files)
+	else state.filesToAdd.push(...files)
+
+	rerender()
 }
 
 const actions = {addFiles}
@@ -28,3 +87,5 @@ const rerender = () => {
 	root = patch(root, diff(tree, newTree))
 	tree = newTree
 }
+
+setTimeout(init, 0)

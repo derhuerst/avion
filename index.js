a7482c448226484df032fb49846f37ffedf3fc93
Jannis R <mail@jannisr.de>
2017-06-14 22:59:02 +0200

    basic UI using virtual-dom

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict'
 
-// todo
+const generateId = require('alphanumeric-id')
+const diff = require('virtual-dom/diff')
+const patch = require('virtual-dom/patch')
+const createElement = require('virtual-dom/create-element')
+
+const render = require('./lib/ui')
+
+const state = {
+	id: location.hash.length === 7 ? location.hash.slice(1) : generateId(6),
+	isLeader: location.hash.length === 7,
+	endpoint: null
+}
+
+const addFiles = (files) => {
+	// todo
+}
+
+const actions = {addFiles}
+
+let tree = render(state, actions)
+let root = createElement(tree)
+document.getElementById('app').appendChild(root)
+
+const rerender = () => {
+	const newTree = render(state, actions)
+	root = patch(root, diff(tree, newTree))
+	tree = newTree
+}
